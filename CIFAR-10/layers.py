bc03267219c2d3fb12b75c6b8348569444767064
Tanguy LE PENNEC <tanguy.le-pennec@isen-ouest.yncrea.fr>
2021-02-17 15:46:56 +0100

    Dense

--- CIFAR-10/layers.py
+++ CIFAR-10/layers.py
@@ -11,15 +11,31 @@ class Convolution():
         pass
 
 class Dense():
+    """
+    Calcul matriciel 
+
+    Exemple avec les dimensions :
+        x.shape      = 4096
+        params.shape = 4096,1000
+        y = 1000
+
+        y_j = x_i*w_i_j + b_j
+
+        x.shape = n
+        params.shape = m*n
+        m < n
+        x*weight => Taille m
+    """ 
     def __init__(self):
+        self.weight = []
         pass 
     def __call__(self):
         pass
-    def load_weight(self):
+    def load_weight(self,new_weight):
         pass 
-    def load_bias(self):
+    def load_bias(self,new_bias):
         pass
-    def load_params(self):
+    def load_params(self,dict_params):
         pass
 
 class ReLU():
@@ -42,7 +58,30 @@ class ReLU():
 
 class Maxpooling():
     """
+    x= [0,1,2,3,4]
+       [5,6,7,8,9]
+       [10,11,12,13,14]
+       [15,16,17,18,19]
 
+    args : 
+        - stride 
+        - kernel_shape
+
+    AVEC STRIDE 1 :
+    [X,X] [0, 1] -> [1,2] -> [2,3] ->  [3,4]
+    [X,X] [5, 6]    [6,7] -> [7,8] ->  [8,9]
+
+    => [5,6]
+       [10,11] ..etc...
+
+    AVEC STRIDE 2 : 
+    [X,X] [0, 1] -> [2,3] 
+    [X,X] [5, 6]    [7,8] 
+
+    OPERATION MAX POOLING: 
+    [1,2]
+    [6,7]
+    MAX = 7
 
     """
     def __init__(self):
